bbfd81ac9ade07df889b30d59e7af6875a7d7660
Jim Lougheed <jims.codes@gmail.com>
2022-09-16 23:32:00 -0400

    Add CrashDetector and FinishLine scripts

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    CircleCollider2D playerHead;
+
+    private void Start() {
+        playerHead = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider)) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
